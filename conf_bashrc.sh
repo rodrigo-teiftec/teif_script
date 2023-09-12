62fbc31f4d9b1dc67f98a2cb41468d79b40c135d
rodrigo-teif <rodrigo@teiftec.com.br>
2023-09-12 12:59:18 -0300

    Conf

--- conf_bashrc.sh
+++ conf_bashrc.sh
@@ -13,7 +13,7 @@ fi
 
 echo "Alterando bashrc ..."
 cat <<FIM > /root/.bashrc
-# *********************************************
+'# *********************************************
 # * ~/.bashrc Personalizado Teiftec             *
 # *                                             *
 # *                                             *
@@ -122,7 +122,7 @@ alias dir='dir --color=auto'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
-fi # Fim do if do dircolor
+fi # Fim do if do dircolor'
 
 FIM
 
